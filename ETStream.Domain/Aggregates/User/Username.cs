5f16c393fc9ccd7b09caf28fdb4fd3cd4a961a05
AndreLuisPLuz <andreluispluz@gmail.com>
2024-09-18 12:57:37 -0300

    checkpoint: created username value object.

---
+++ ETStream.Domain/Aggregates/User/Username.cs
@@ -0,0 +1,12 @@
+namespace ETStream.Domain.Aggregates.User
+{
+    public class Username
+    {
+        public string value { get; private set; }
+
+        public Username(string name)
+        {
+            
+        }
+    }
+}
