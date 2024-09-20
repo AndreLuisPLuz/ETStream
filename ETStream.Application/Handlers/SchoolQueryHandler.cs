9b745e376f3ae25e1eec2f54f04b73f09500b7a3
AndreLuisPLuz <andreluispluz@gmail.com>
2024-09-20 16:39:57 -0300

    feat: created concrete query for fetching school details, assigned it to new school query handler.

---
+++ ETStream.Application/Handlers/SchoolQueryHandler.cs
@@ -0,0 +1,30 @@
+using ETStream.Application.Queries;
+using ETStream.Application.Seed;
+using ETStream.Domain.Aggregates.School;
+using ETStream.Domain.Seed;
+
+namespace ETStream.Application.Handlers
+{
+    public class SchoolQueryHandler : IQueryHandler<SchoolDetails?, GetSchoolDetailsProps>
+    {
+        private IRepository<SchoolEntity> _repository;
+
+        public SchoolQueryHandler(IRepository<SchoolEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<SchoolDetails?> HandleAsync(Query<GetSchoolDetailsProps, SchoolDetails?> query)
+        {
+            var school = await _repository.FindByIdAsync(new Guid(query.Properties.SchoolId));
+            
+            if (school is null)
+                return null;
+            
+            return new SchoolDetails
+            {
+                Description = school.Description
+            };
+        }
+    }
+}
---
+++ ETStream.Application/Queries/GetSchoolDetails.cs
@@ -0,0 +1,21 @@
+using ETStream.Application.Seed;
+
+namespace ETStream.Application.Queries
+{
+    public readonly record struct GetSchoolDetailsProps
+    {
+        public required readonly string SchoolId { get; init; }
+    }
+
+    public readonly record struct SchoolDetails
+    {
+        public readonly string Description { get; init; }
+    }
+
+    public class GetSchoolDetails : Query<GetSchoolDetailsProps, SchoolDetails>
+    {
+        public GetSchoolDetails(GetSchoolDetailsProps props) : base(props)
+        {
+        }
+    }
+}
