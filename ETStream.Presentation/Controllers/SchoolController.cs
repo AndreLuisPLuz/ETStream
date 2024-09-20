3cd6d9cdf4a155ad437d2c839bfaf0a599e72fc7
janeckiisa <janeckiisabelle@gmail.com>
2024-09-20 14:58:38 -0300

    feat: created school controller, added school creation route.

---
+++ ETStream.Presentation/Controllers/SchoolController.cs
@@ -0,0 +1,24 @@
+using ETStream.Application.Commands;
+using ETStream.Application.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETStream.Presentation.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/school")]
+    public class SchoolController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult> CreateSchool(
+                [FromServices] SchoolCommandHandler handler,
+                [FromBody] CreateSchoolCommand command)
+        {
+            var schoolId = await handler.HandleAsync(command);
+
+            if (schoolId is null)
+                return BadRequest();
+            
+            return Created();
+        }
+    }
+}
